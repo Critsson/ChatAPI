6457f625fc4849732d753111dc90c184a99ada14
Chris <chris.gao77@gmail.com>
2023-02-25 22:32:23 -0500

    Added some authentication

--- server/server.js
+++ server/server.js
@@ -3,10 +3,12 @@ const { createServer } = require("http");
 const { Server } = require("socket.io");
 const bcrypt = require("bcrypt")
 const cors = require("cors")
-const {MongoClient, ServerApiVersion} = require("mongodb")
+const { MongoClient, ServerApiVersion } = require("mongodb")
 require("dotenv").config({ path: "./.env.local" })
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@chatcluster.2n7cumj.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const jwt = require("jsonwebtoken")
+const cookieParser = require("cookie-parser")
 const saltRounds = 12;
 
 const app = express();
@@ -18,12 +20,32 @@ const io = new Server(httpServer, {
 });
 
 
-app.use(cors())
+app.use(cors({ credentials: true }))
+app.use(cookieParser())
 app.use(express.json())
+
+//Middleware that verifies a JSON web token
+const verifyJwt = async (req, res, next) => {
+    const token = req.cookies.jwt
+
+    if (!token) {
+        return res.status(401).send({ message: "No token" })
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: "Not authorized" })
+        }
+        req.user = decoded
+    })
+
+    next()
+
+}
 
 //Register user to database
 app.post("/register", async (req, res) => {
-    const {username, password} = req.body
+    const { username, password } = req.body
 
     try {
         const generatedSalt = await bcrypt.genSalt(saltRounds)
@@ -43,12 +65,16 @@ app.post("/register", async (req, res) => {
             "password": hashedPass,
             "chats": []
         }
-        
+
         const p = await users.insertOne(userDocument)
-        res.status(200).send("User successfully registered")
+        const token = jwt.sign({ id: p.insertedId, username }, process.env.JWT_SECRET, {
+            expiresIn: "24h"
+        })
+        res.cookie("jwt", token, { maxAge: 86400000, httpOnly: true })
+        return res.status(200).send("User successfully registered")
     } catch (error) {
         console.error(error)
-        if(error.code === 11000) {
+        if (error.code === 11000) {
             res.status(500).send("Username already exists")
         } else {
             res.status(500).send("Failed to register to database")
@@ -58,18 +84,24 @@ app.post("/register", async (req, res) => {
     }
 })
 
+
+//Login user
 app.post("/login", async (req, res) => {
-    const {username, password} = req.body
+    const { username, password } = req.body
 
     try {
         await client.connect()
         const database = client.db("ChatApp")
         const users = database.collection("Users")
 
-        const user = await users.findOne({username})
-        if(user) {
+        const user = await users.findOne({ username })
+        if (user) {
             const isValidPass = await bcrypt.compare(password, user.password)
-            if(isValidPass) {
+            if (isValidPass) {
+                const token = jwt.sign({ id: user._id, username }, process.env.JWT_SECRET, {
+                    expiresIn: "24h"
+                })
+                res.cookie("jwt", token, { maxAge: 86400000, httpOnly: true })
                 res.status(200).send("Authenticated")
             } else {
                 res.status(500).send("Incorrect password")
@@ -106,13 +138,13 @@ app.get("/api/users", async (req, res) => {
 app.get("/api/users/:username", async (req, res) => {
     try {
         await client.connect()
-        const {username} = req.params
+        const { username } = req.params
         const database = client.db("ChatApp")
         const users = database.collection("Users")
 
-        const user = await users.findOne({username})
+        const user = await users.findOne({ username })
 
-        if(user) {
+        if (user) {
             res.status(200).send(user)
         } else {
             res.status(500).send("User does not exist")
